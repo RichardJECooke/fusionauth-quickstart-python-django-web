1f0c5db36da643905d39b29cd98e3249d25cd4d4
RichardJECooke <RichardJECooke@pm.me>
2023-08-06 18:24:48 +0200

    got new urls to work

--- complete-application/mysite/app/views.py
+++ complete-application/mysite/app/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, template_name='app/authentication.html')
+def app(request):
+    return render(request, template_name='home.html')
+
+def login(request):
+    return render(request, template_name='account.html')
+
+def change(request):
+    return render(request, template_name='make-change.html')
