c2fe5b221c0b87a95a9fdbcdf5c15ca77fee40b3
RichardJECooke <RichardJECooke@pm.me>
2023-08-07 12:03:28 +0200

    Everything working except user email from FA

--- complete-application/mysite/app/urls.py
+++ complete-application/mysite/app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.app, name='app'),
     path('account/', views.account, name='account'),
-    path('make-change/', views.change, name='change'),
+    path('logout/', views.logout, name='logout'),
+    path('change/', views.change, name='change'),
 ]
